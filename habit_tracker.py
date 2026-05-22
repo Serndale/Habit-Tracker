c936f13de8147aaab438f26bfa3c4c68a4d4ba1a
Dustin Ramirez <dustin.ramirez03@gmail.com>
2026-05-21 17:28:55 -0700

    Added marking habits complete. Added notes

--- habit_tracker.py
+++ habit_tracker.py
@@ -2,25 +2,38 @@
 import sys
 habits = []
 
-while True:
+while True: # main loop
 
-    print('Habit Tracker')
+    print('Habit Tracker') # main program options
     print('1. Add Habit')
     print('2. View Habits')
     print('3. Mark Habit Complete')
     print('4. Quit')
 
-    choice = input('Choose an option: ')
+    choice = input('Choose an option: ') 
     if choice == '4':
-        break
+        break # exit program
 
 
-    if choice == '1':
+    if choice == '1': # input habit name and add to list
         habit_name = input('Enter habit name: ')
-        habits = habits + [habit_name]
+        habit = {
+            "name": habit_name,
+            "completed": False # habit is not completed when added
+        }
+        habits.append(habit)
         print('Habit added.')
-    elif choice == '2':
+    elif choice == '2': # view habits and completion status
         print('Your habits:')
-        for habit_name in habits:
-            print(habit_name)
-    
+        for habit in habits:
+            print(habit["name"] + ' - ' + ('Completed' if habit['completed'] else 'Not Completed'))
+    elif choice == '3': # menu for marking habits complete. shows habits and completion status to update.
+        print('Your habits:')
+        for habit in habits:
+            print(habit['name'] + ' - ' + ('Completed' if habit['completed'] else 'Not Completed'))
+        habit_name = input('Enter habit name to mark complete: ')
+        for habit in habits:
+            if habit['name'] == habit_name:
+                habit['completed'] = True
+                print('Habit marked complete.')
+                
